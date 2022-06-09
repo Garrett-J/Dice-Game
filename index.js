0e44536a03abd37fa90f91a823076c1f7c24aac8
Garrett Johnson <89829159+Garrett-J@users.noreply.github.com>
2022-06-09 12:06:30 -0700

    cleaning up code

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
+//Set variables for the two players
 var playerOne = "Player One";
 var playerTwo = "Player Two";
 
-
-function changeNames(){
-    playerOne = prompt("Enter Player 1's name.");
-    playerTwo = prompt("Enter Player 2's name.");
-    document.getElementById("player1").innerHTML = playerOne;
-    document.getElementById("player2").innerHTML = playerTwo;
+//Prompt the user to enter their own names for the two player variables
+function changeNames() {
+  playerOne = prompt("Enter Player 1's name.");
+  playerTwo = prompt("Enter Player 2's name.");
+  document.getElementById("player1").innerHTML = playerOne;
+  document.getElementById("player2").innerHTML = playerTwo;
 }
 
-function rollDice(){
-    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-    
-    document.getElementById("dice1").setAttribute("src", "images/Dice" + randomNumber1 + ".png");
-    document.getElementById("dice2").setAttribute("src", "images/Dice" + randomNumber2 + ".png");
+//Roll two random numbers between 1 and 6, show corresponding images on each dice, declare the winner depending on which is larger.
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-    if (randomNumber1 === randomNumber2) {
-        document.querySelector("h1").innerHTML = "Draw!"
+  document
+    .getElementById("dice1")
+    .setAttribute("src", "images/Dice" + randomNumber1 + ".png");
+  document
+    .getElementById("dice2")
+    .setAttribute("src", "images/Dice" + randomNumber2 + ".png");
 
-    } else if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").innerHTML = (playerOne + " wins!")
-    } else {
-        document.querySelector("h1").innerHTML = (playerTwo + " wins!")
-    }
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = playerOne + " wins!";
+  } else {
+    document.querySelector("h1").innerHTML = playerTwo + " wins!";
+  }
 }
-
-
-
